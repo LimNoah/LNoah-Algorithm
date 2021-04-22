ecce2d7580c6f4c00f25aa2066f42eeb873a531a
LNoah <limnoah0301@gmail.com>
2021-04-22 17:45:47 +0900

    solve: Solved no.2447.

--- Baekjoon/src/B2447/Main.java
+++ Baekjoon/src/B2447/Main.java
@@ -14,10 +14,10 @@ public class Main {
 
         stars = new char[N][N];
 
-        pattern(N);
+        pattern(N, 0, 0, false);
 
-        for(int i = 0; i < stars.length; i++){
-            for(int j = 0; j < stars[0].length; j++){
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
                 sb.append(stars[i][j]);
             }
             sb.append("\n");
@@ -25,10 +25,31 @@ public class Main {
 
         System.out.println(sb);
     }
-    public static void pattern(int N){
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
 
+    public static void pattern(int N, int x, int y, boolean isBlank){
+        if(isBlank){
+            for(int i = x; i < x + N; i++){
+                for(int j = y; j < y + N; j++){
+                    stars[i][j] = ' ';
+                }
+            }
+            return;
+        }
+
+        if(N == 1){
+            stars[x][y] = '*';
+            return;
+        }
+
+        int cnt = 0;
+        for(int i = x; i < x + N; i += N / 3){
+            for(int j = y; j < y + N; j += N / 3){
+                if(cnt == 4){
+                    pattern(N / 3, i, j, true);
+                } else {
+                    pattern(N / 3, i, j, false);
+                }
+                cnt++;
             }
         }
     }
