725568f83120b6bc5b9fdb00833594070294f477
LNoah <limnoah0301@gmail.com>
2021-05-07 04:38:59 +0900

    solve: Solved 1874.

--- Baekjoon/src/B1874/Main.java
+++ Baekjoon/src/B1874/Main.java
@@ -3,9 +3,6 @@ package B1874;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Stack;
 
 public class Main {
@@ -36,7 +33,6 @@ public class Main {
             sb.append("-\n");
             n--;
         }
-
         System.out.println(sb);
     }
 }
