25c4672434a1adfcd733d58213f93697c43ed121
LNoah <limnoah0301@gmail.com>
2021-05-06 20:38:03 +0900

    solve: Solved a problem. Need to resolve.

---
+++ Baekjoon/src/B1874/Main.java
@@ -0,0 +1,42 @@
+package B1874;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+
+        int n = Integer.parseInt(br.readLine());
+
+        int start = 0;
+
+        while(n > 0){
+            int value = Integer.parseInt(br.readLine());
+
+            if(value > start){
+                for(int i = start + 1; i <= value; i++){
+                    stack.push(i);
+                    sb.append("+\n");
+                }
+                start = value;
+            } else if(stack.peek() != value){
+                System.out.println("NO");
+                return;
+            }
+
+            stack.pop();
+            sb.append("-\n");
+            n--;
+        }
+
+        System.out.println(sb);
+    }
+}
