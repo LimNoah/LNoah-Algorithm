782a0d0a02631ce4516b4be7f3816de7bf7c1f0c
LNoah <limnoah0301@gmail.com>
2021-05-11 03:11:32 +0900

    solve: Solved 1966. Used ProirityQueue.

--- Baekjoon/src/B1966/Main.java
+++ Baekjoon/src/B1966/Main.java
@@ -3,11 +3,12 @@ package B1966;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
 
@@ -18,7 +19,31 @@ public class Main {
 
             st = new StringTokenizer(br.readLine());
 
+            Queue<Integer> queue = new LinkedList<>();
+            PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+            int printCnt = 0;
 
+            while (st.hasMoreTokens()){
+                int value = Integer.parseInt(st.nextToken());
+                queue.offer(value);
+                priorityQueue.offer(value);
+            }
+
+            while(true){
+                if(queue.peek() < priorityQueue.peek()){
+                    queue.offer(queue.poll());
+                } else {
+                    queue.poll();
+                    priorityQueue.poll();
+                    printCnt++;
+                    if(M == 0){
+                        break;
+                    }
+                }
+                M += (M - 1) < 0 ? queue.size() - 1 : -1;
+            }
+            sb.append(printCnt).append("\n");
         }
+        System.out.println(sb);
     }
 }
