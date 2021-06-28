3d9966d4f9fc1200a7bcd80e4cd7b9177de13a77
LNoah <limnoah0301@gmail.com>
2021-06-28 16:58:17 +0900

    solve: Solved problem.

---
+++ Baekjoon/src/B5430/Main.java
@@ -0,0 +1,14 @@
+package B5430;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+    }
+}
---
+++ Programmers/src/연습문제/Level1/행렬의덧셈/Main.java
@@ -0,0 +1,13 @@
+package 연습문제.Level1.행렬의덧셈;
+
+public class Main {
+    public static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        for(int i = 0; i < arr1.length; i++){
+            for(int j = 0; j < arr1[0].length; j++){
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        return answer;
+    }
+}
