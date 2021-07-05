63076b39c3254feb71d38e0e76bc93c7771fd861
LNoah <limnoah0301@gmail.com>
2021-07-05 21:07:33 +0900

    solve: Solved a problem.

--- Programmers/src/연습문제/Level1/정수내림차순으로배치하기/Solution.java
+++ Programmers/src/연습문제/Level1/정수내림차순으로배치하기/Solution.java
@@ -1,18 +1,22 @@
 package 연습문제.Level1.정수내림차순으로배치하기;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 class Solution {
     public long solution(long n) {
         long answer = 0;
-        String longStr = Long.toString(n);
-        int length = longStr.length();
-        Character[] characters = new Character[length];
-        for(int i = 0; i < length; i++){
-            characters[i] = longStr.charAt(i);
+        String longToStr = Long.toString(n);
+        Character[] characters = new Character[longToStr.length()];
+        for(int i = 0; i < longToStr.length(); i++){
+            characters[i] = longToStr.charAt(i);
         }
-        Arrays.sort(characters);
-        Arrays.
+        Arrays.sort(characters, Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+        for (Character character : characters) {
+            sb.append(character);
+        }
+        answer = Long.parseLong(sb.toString());
         return answer;
     }
 }
