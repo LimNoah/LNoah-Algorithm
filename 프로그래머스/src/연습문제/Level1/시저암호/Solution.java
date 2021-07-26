c7f27326850f933a47568adc7c14475c5f2074d3
LNoah <limnoah0301@gmail.com>
2021-07-26 22:27:14 +0900

    solve: Solved a problem

---
+++ 프로그래머스/src/연습문제/Level1/시저암호/Solution.java
@@ -0,0 +1,24 @@
+package 연습문제.Level1.시저암호;
+
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            char inputChar = s.charAt(i);
+            if(!(s.charAt(i) == 32)){
+                inputChar += n;
+            }
+            if(s.charAt(i) >= 97 && s.charAt(i) <= 122){
+                if(s.charAt(i) + n > 122){
+                    inputChar -= 26;
+                }
+            } else if(s.charAt(i) >= 65 && s.charAt(i) <= 90){
+                if(s.charAt(i) + n > 90){
+                    inputChar -= 26;
+                }
+            }
+            sb.append(inputChar);
+        }
+        return sb.toString();
+    }
+}
