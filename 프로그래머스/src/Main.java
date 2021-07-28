44558cbad5e9fc583a6e89a388d0ab9a99675044
LNoah <limnoah0301@gmail.com>
2021-07-28 22:31:22 +0900

    docs: Create Main.java for test Solution methods

---
+++ 프로그래머스/src/Main.java
@@ -0,0 +1,8 @@
+import 연습문제.Level1.하샤드수.Solution;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(10));
+    }
+}
