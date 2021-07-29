1e5ee2126026afd6937f461cfa48383df33763c5
LNoah <limnoah0301@gmail.com>
2021-07-29 16:34:50 +0900

    docs: Update Main.java

--- 프로그래머스/src/Main.java
+++ 프로그래머스/src/Main.java
@@ -1,8 +1,10 @@
-import 연습문제.Level1.하샤드수.Solution;
+import 연습문제.Level1.최대공약수와최소공배수.Solution;
 
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.solution(10));
+        for(int i = 0; i < 2; i++){
+            System.out.println(solution.solution(2, 5)[i]);
+        }
     }
 }
