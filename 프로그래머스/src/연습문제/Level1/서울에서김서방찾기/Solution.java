80c29b927c55090b8b0e55f9ccc7238bbf4e15f3
LNoah <limnoah0301@gmail.com>
2021-07-29 16:35:51 +0900

    solve: Solved a problem

---
+++ 프로그래머스/src/연습문제/Level1/서울에서김서방찾기/Solution.java
@@ -0,0 +1,17 @@
+package 연습문제.Level1.서울에서김서방찾기;
+
+public class Solution {
+    public String solution(String[] seoul) {
+        StringBuilder sb = new StringBuilder();
+        int idx = 0;
+
+        for(int i = 0; i < seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                idx = i;
+            }
+        }
+
+        sb.append("김서방은 ").append(idx).append("에 있다");
+        return sb.toString();
+    }
+}
