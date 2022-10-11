c8e527595f06a82be1049a9132d22b5a8152a24d
LimNoah <limnoah0301@gmail.com>
2022-10-11 09:41:26 +0900

    solve: Solved a problem

---
+++ 프로그래머스/src/Practice/Level1/Java/나머지가1이되는수찾기/Solution.java
@@ -0,0 +1,14 @@
+package Practice.Level1.Java.나머지가1이되는수찾기;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 2; i <= n; i++){
+            if(n%i == 1) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
