feac0228fa4a5225801fc924d5a68f28b35b3107
LimNoah <limnoah0301@gmail.com>
2022-10-26 09:12:54 +0900

    solve: Solved a problem

---
+++ 프로그래머스/src/Practice/Level0/Java/배열원소의길이/Solution.java
@@ -0,0 +1,11 @@
+package Practice.Level0.Java.배열원소의길이;
+
+class Solution {
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        for(int i = 0; i < strlist.length; i++) {
+            answer[i] = strlist[i].length();
+        }
+        return answer;
+    }
+}
