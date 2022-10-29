75b900e1497b0d1471dd13a12eb96708bf558c3a
LimNoah <limnoah0301@gmail.com>
2022-10-30 00:10:55 +0900

    solve: Solved a problem

---
+++ 프로그래머스/src/Practice/Level0/Java/아이스아메리카노/Solution.java
@@ -0,0 +1,8 @@
+package Practice.Level0.Java.아이스아메리카노;
+
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = {money / 5500, money % 5500};
+        return answer;
+    }
+}
