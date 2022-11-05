e18dd009e274348de827ac415b1a627a33906081
LimNoah <limnoah0301@gmail.com>
2022-11-06 02:55:53 +0900

    solve: Solved a problem

---
+++ 프로그래머스/src/Practice/Level0/Java/문자열정렬하기2/Solution.java
@@ -0,0 +1,13 @@
+package Practice.Level0.Java.문자열정렬하기2;
+
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+
+        Arrays.sort(my_string.toCharArray());
+
+        return answer;
+    }
+}
