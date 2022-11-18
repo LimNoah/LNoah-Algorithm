2d049bf32494bfe44da709ef99b2d616f50cb207
LimNoah <limnoah0301@gmail.com>
2022-11-18 11:44:58 +0900

    docs: Added test code

--- 프로그래머스/src/Main.java
+++ 프로그래머스/src/Main.java
@@ -1,9 +1,10 @@
-import Practice.Level1.Java._1차_다트게임.Solution;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.solution("1D2S3T*"));
-        System.out.println("0123456".substring(2));
     }
 }
